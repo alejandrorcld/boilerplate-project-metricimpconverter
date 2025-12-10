83a4805b49627fa0f65371f59f13fdbb6e2ba272
Alejandro <tu_correo@ejemplo.com>
2025-12-10 20:14:21 -0300

    commit

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,13 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
+  test('Debe leer un número entero', function() {
+    assert.equal(convertHandler.getNum('32L'), 32);
+  });
 
+  test('Debe leer un número decimal', function() {
+    assert.equal(convertHandler.getNum('3.2L'), 3.2);
+  });
+
+  // … y así hasta cubrir todos los casos
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,16 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+  test('Convertir 10L (GET)', function(done) {
+    chai.request(server)
+      .get('/api/convert')
+      .query({input: '10L'})
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.initNum, 10);
+        assert.equal(res.body.initUnit, 'L');
+        assert.equal(res.body.returnUnit, 'gal');
+        done();
+      });
+  });
 });
